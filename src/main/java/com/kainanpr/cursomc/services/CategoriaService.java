9959689c8b59c5ff594853faa76d1f4deaf77830
Kainanpr <Kainanpr95@gmail.com>
2018-03-28 17:34:43 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/kainanpr/cursomc/services/CategoriaService.java
+++ src/main/java/com/kainanpr/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.kainanpr.cursomc.domain.Categoria;
 import com.kainanpr.cursomc.repositories.CategoriaRepository;
+import com.kainanpr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,12 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
+		
 		return obj;
 	}
 	
